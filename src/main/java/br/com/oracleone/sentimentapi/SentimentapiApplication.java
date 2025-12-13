55be00a4fd4148b138d328aee5eb652e1e7283b2
keila <keilanunes046@gmail.com>
2025-12-12 21:56:12 -0300

    Adiciona controller de SentimentAPI

--- src/main/java/br/com/oracleone/sentimentapi/SentimentapiApplication.java
+++ src/main/java/br/com/oracleone/sentimentapi/SentimentapiApplication.java
@@ -4,10 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SentimentapiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SentimentapiApplication.class, args);
-	}
 
 }
